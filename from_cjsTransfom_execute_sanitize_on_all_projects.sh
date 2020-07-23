9626b1122a7b9bdba8ba394783b7fc2e5f146b39
sam <raphael.s@ccs.neu.edu>
2020-07-23 15:19:49 -0400

    added to gitignore, projects etc

--- from_cjsTransfom_execute_sanitize_on_all_projects.sh
+++ from_cjsTransfom_execute_sanitize_on_all_projects.sh
@@ -1,1 +1,5 @@
-./clean ; for file in `ls  /Users/sam/Dropbox/Spring_20/research_proj/CJS_Transform/real_projects `; do src/transformations/sanitizing/sanitize_project.ts  ./real_projects/$file ./real_san/$file  ;done;
+./clean ;
+
+ for file in `ls  /Users/sam/Dropbox/Spring_20/research_proj/CJS_Transform/real_projects `; 
+ do src/transformations/sanitizing/sanitize_project.ts  ./real_projects/$file ./real_san/$file  ;
+ done;
