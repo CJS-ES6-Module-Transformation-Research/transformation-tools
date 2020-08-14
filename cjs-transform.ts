9b6216f6bbbf5eddaeddd2806cd883f972a6c7f9
sam <raphael.s@ccs.neu.edu>
2020-08-14 19:46:29 -0400

    cjs-tf

--- cjs-transform.ts
+++ cjs-transform.ts
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ts-node
 import {isAbsolute, join} from "path";
 import * as yargs from "yargs";
-import {Arguments, Argv, CommandModule} from "yargs";
+import {Argv, CommandModule, InferredOptionType, InferredOptionTypes} from "yargs";
 import {write_status} from "./src/abstract_fs_v2/interfaces";
 import {ProjConstructionOpts, ProjectManager} from "./src/abstract_fs_v2/ProjectManager";
 import executioner from "./src/executioner";
@@ -9,25 +9,24 @@ import executioner from "./src/executioner";
 
 const cwd = process.cwd()
 
-// test_resources.import path from 'path'
-
-let lit = {demandOption: true, choices: ["named", "default"], nargs: 1}
-type opt = { demandOption: true, choices: ["named", "default"], nargs: 1 }
 
 type naming = "default" | "named"
-let {input, output, suffix, operation, naming_format} = getOptionData(yargs
-	.command(copyCommandModule())
-	.command(inPlaceCommandModule())
-	.option("named", {type: "boolean"})
-	.option("default", {type: "boolean"})
-	.strict()
-	.argv);
-// .option('named',{type:"string", choices:["named","default"]})
+let {input, output, suffix, operation, naming_format,ignored } = getOptionData();
 
 
 executioner(new ProjectManager(input,
-	getProjConstructionOpts(suffix, output, operation,naming_format)))
+	getProjConstructionOpts(suffix, output, operation, naming_format,ignored)))
 
+// interface _named{
+// 	named:
+// }
+// interface  x extends InferredOptionTypes<any> {
+// 	named?:{type: "boolean"}
+// 	'default'?:{type: "boolean"}
+// 	ignored?: {type: "string"}
+//
+// }
+// type PA = x & ProgramArgs
 
 export interface ProgramArgs {
 	source: string
@@ -37,26 +36,43 @@ export interface ProgramArgs {
 	// naming?:naming
 	named?: boolean
 	'default'?: boolean
+	// ignored?: string[]
+	// import_type?:naming
 }
 
-export function getProjConstructionOpts(suffix, output, operation, naming_format: "default" | "named"): ProjConstructionOpts {
+export function getProjConstructionOpts(suffix, output, operation, naming_format: "default" | "named",ignored ): ProjConstructionOpts {
 	return {
 		isModule: false,
 		suffix: suffix,
 		target_dir: output,
 		write_status: operation,
 		copy_node_modules: false, //TODO
-		isNamed: naming_format === "named"
+		isNamed: naming_format === "named",
+		// ignored
 	};
 }
 
-function getOptionData(tf_args: Arguments<ProgramArgs>) {
+function getOptionData() {
+	let tf_args = yargs
+		.command(copyCommandModule())
+		.command(inPlaceCommandModule())
+		.option('import_type',   {choices: ["named", "default"], nargs: 1})
+		.option(	'ignored', {type: "string", array: true})
+		// .option({
+		// 	import_type: {choices: ["named", "default"], nargs: 1},
+		//
+		// 	ignored: {type: "string", array: true} )
+		.strict()
+		.argv
 	let input: string = tf_args.source;
 	let output = '';
 	let suffix = '';
 	let operation: write_status
-	let naming_format: naming = "default";
+	// @ts-ignore
+	let naming_format: naming = tf_args.import_type  ? tf_args.import_type  : "default";
 
+	// @ts-ignore
+	let ignored:string[] = tf_args.ignored ? tf_args.ignored:[] ;
 	if (tf_args._[0] === "tf-proj") {
 		operation = "in-place";
 		suffix = tf_args.suffix
@@ -67,24 +83,17 @@ function getOptionData(tf_args: Arguments<ProgramArgs>) {
 			output = join(cwd, output)
 		}
 	}
-	if (tf_args.named && tf_args.default) {
-		console.log(`Please choose ONE name specification method '--named' or '--default'`)
-		process.exit(1)
-
-	} else if (tf_args.named)  {
-		naming_format = "named"
-		console.log('naming is ' + naming_format)
-
-	} else if (tf_args.default){
-		naming_format = "default"
-		console.log('naming is ' + naming_format)
-
-	}
 
 	if (!isAbsolute(input)) {
 		input = join(cwd, input)
 	}
-	return {input, output, suffix, operation, naming_format};
+	ignored.forEach((elem, index,arr)=>{
+		if (!isAbsolute(elem)){
+			arr[index] = join(cwd, elem )
+		}
+	});
+
+	return {input, output, suffix, operation, naming_format, ignored};
 }
 
 function copyCommandModule(): CommandModule<ProgramArgs, ProgramArgs> {
@@ -95,8 +104,8 @@ function copyCommandModule(): CommandModule<ProgramArgs, ProgramArgs> {
 		command: "tf-copy <source> <dest>",
 		builder: (args: Argv<ProgramArgs>): Argv<ProgramArgs> => {
 			// .option('filter',{})
-			return args
-				.option('include-node_modules', {})
+			return optionized(args)
+				// .option('include-node_modules', {type:"boolean"})
 
 		},
 		aliases: 'c',
@@ -111,12 +120,24 @@ function copyCommandModule(): CommandModule<ProgramArgs, ProgramArgs> {
 	}
 }
 
+// type infOp = InferredOptionTypes<_Options>
+function optionized(args: yargs.Argv<ProgramArgs>) {
+	let _args
+	 	= args.option({
+		import_type: {choices: ["named", "default"], nargs: 1},
+
+		ignored: {type: "string", array: true}
+	});
+	return _args
+}
+
 function inPlaceCommandModule(): CommandModule<ProgramArgs, ProgramArgs> {
 	// const inPlaceCommandModule: CommandModule<ProgramArgs, ProgramArgs> =
 	return {
 		command: "tf-proj <source> [suffix]",
 		builder: (args: Argv<ProgramArgs>): Argv<ProgramArgs> => {
-			return args.option('', {});
+			return  args
+			//optionized(args);
 		},
 		aliases: 'i',
 		describe: "Executes a transformation from CommonJS modules to ECMAScript modules on a project directory in-place.",
