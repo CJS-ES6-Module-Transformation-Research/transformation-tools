d98e38e4c0d26507a1e92fb92c47c78cd64b785b
sam <raphael.s@ccs.neu.edu>
2020-08-21 10:59:37 -0400

    more fixes

--- Utils/clean_proj.ts
+++
@@ -1,7 +0,0 @@
-const binary = `/Users/sam/Dropbox/Spring_20/research_proj/CJS_Transform/clean`
-import {execFileSync} from 'child_process'
-
-export function cleanProject() {
-    return execFileSync(binary).toString();
-}
-
