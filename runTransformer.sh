efea3f7f76927134fabbc5d96cdb0e43ac834525
sam raphael <samraphael@gmail.com>
2021-08-06 15:07:17 -0400

    change to runTransformer

--- runTransformer.sh
+++ runTransformer.sh
@@ -2,9 +2,13 @@
 
 transformRoot=$1
 
+if [ -z $transformRoot ]; then
+    transformRoot="."
+fi
+shift
+
 if [ ! -d "$transformRoot" ]; then
 	echo "Usage: ./runTransformer.sh <directory-to-transform>"
 	exit
 fi
-
-ts-node cjs-transform.ts i --import_type named $transformRoot --report --ignored dist
+node ${PROJECT_ROOT}/out.d/cjs-transform.ts i --import_type named $transformRoot $2 --ignored dist  configs rolls webpack-bundles
